11878e5b8f3b8410ee73a67ac969a6127148ed38
Aaron K. Clark <akclark@atbashservices.com>
2020-12-01 07:35:24 -0600

    First Push

---
+++ D01/d01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace d01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null)
+                Console.WriteLine("Enter filename to parse numbers!");
+            else
+                parseFile(args);
+        }
+        static void parseFile(string[] fileName)
+        {
+            // do something with file
+        }
+    }
+}

f1606e33623f302ac58b7aba6024da0d792dc52b
CryptoJones <cryptojones@gmail.com>
2020-12-24 03:33:52 -0600

    Day Two

---
+++ D02/D02/PairedValues.cs
@@ -0,0 +1,15 @@
+    public class PairedValues
+    {
+        // These are just simple ways of creating a getter and setter in c#
+        public string value1 { get; set; }
+        public string value2 { get; set; }
+        public string value3 { get; set; }
+
+        // A constructor which sets all your getters and setters
+        public PairedValues(string Value1, string Value2, string Value3)
+        {
+            value1 = Value1;
+            value2 = Value2;
+            value3 = Value3;
+        }
+    }
---
+++ D02/D02/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace D02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (args == null || args)
+                Console.WriteLine("Enter filename to parse numbers!");
+            else
+                foreach (string value in args)
+                {
+                    sb.Append(value);
+                }
+            partOneValidPasswordCount(sb.ToString());
+            //partTwoParseFile(sb.ToString());
+        }
+
+        public static int partOneValidPasswordCount(string fileName)
+        {
+            // Method Stuff
+            int result = 0;
+            
+
+            // File Processing
+            List<string> lineList = new List<string>();
+            int lineListCounter = 0;
+            string line;
+
+            System.IO.StreamReader file = new System.IO.StreamReader(fileName);
+            while ((line = file.ReadLine()) != null)
+            {
+                lineList.Add((line));
+                lineListCounter++;
+            }
+
+            List<PairedValues> elements = new List<PairedValues>();
+
+            for(int i = 0; i < lineList.Count +1; i++){
+                string[] subs = lineList[i].ToString().Split(' ');
+                PairedValues pv = new PairedValues(subs[0], subs[1], subs[2]);
+                elements.Add(pv);
+            }
+
+
+            return result;
+        }
+    }
+}

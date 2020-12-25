f6628fe291100c7c2d89694a574938d279d5ab38
CryptoJones <cryptojones@gmail.com>
2020-12-25 05:48:16 -0600

    Day 2 Complete

--- D02/D02/Program.cs
+++ D02/D02/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace D02
@@ -8,29 +9,106 @@ namespace D02
     {
         static void Main(string[] args)
         {
+            
+            
             StringBuilder sb = new StringBuilder();
-            if (args == null || args)
+            if (args == null || !(args.Length > 0))
                 Console.WriteLine("Enter filename to parse numbers!");
             else
                 foreach (string value in args)
                 {
                     sb.Append(value);
                 }
-            partOneValidPasswordCount(sb.ToString());
-            //partTwoParseFile(sb.ToString());
+            //Console.WriteLine("There were {0} instances of correct passwords for part one.", partOneValidPasswordCount(sb.ToString()).ToString());
+            Console.WriteLine("There were {0} instances of correct passwords for part two.", partTwoValidPasswordCount(sb.ToString()).ToString());
+
         }
 
         public static int partOneValidPasswordCount(string fileName)
         {
-            // Method Stuff
-            int result = 0;
-            
 
-            // File Processing
+            // Object to hold the lines of the file
+            List<string> lineList = GetLineList(fileName);
+
+            // object to hold elements from each line
+            List<PairedValues> pairedValuesList = GetPairedValuesList(lineList);
+            
+            // object to hold the password records
+            List<PasswordRecord> passwordRecords = GeneratePasswordRecords(pairedValuesList);
+
+            // return result
+            return countValidPasswords(passwordRecords);
+        }
+
+        public static int partTwoValidPasswordCount(string fileName)
+        {
+
+            // Object to hold the lines of the file
+            List<string> lineList = GetLineList(fileName);
+
+            // object to hold elements from each line
+            List<PairedValues> pairedValuesList = GetPairedValuesList(lineList);
+
+            // object to hold the password records
+            List<PasswordRecord> passwordRecords = GeneratePasswordRecords(pairedValuesList);
+
+            // return result
+            return CountActualValidPasswords(passwordRecords);
+        }
+
+        private static int CountActualValidPasswords(List<PasswordRecord> passwordRecords)
+        {
+            int count = 0;
+
+            foreach (PasswordRecord pr in passwordRecords)
+            {
+                if ((pr.Password[pr.MinValue - 1] == pr.LetterRequired) || (pr.Password[pr.MaxValue -1] == pr.LetterRequired))
+                {
+                    if ((pr.Password[pr.MinValue - 1] == pr.LetterRequired) &
+                        (pr.Password[pr.MaxValue - 1] == pr.LetterRequired))
+                    {
+
+                    }
+                    else
+                    {
+                        count++;
+
+                    }
+                    Console.WriteLine(string.Format("{0}, {1}, {2}, {3}", pr.MinValue, pr.MaxValue, pr.LetterRequired, pr.Password));
+
+                }
+
+            }
+
+            return count;
+        }
+
+        private static int countValidPasswords(List<PasswordRecord> passwordRecords)
+        {
+            int count = 0;
+            
+            foreach (PasswordRecord pr in passwordRecords)
+            {
+                int letterCount = pr.Password.Split(pr.LetterRequired).Length - 1;
+                if ((letterCount <= pr.MaxValue) & (letterCount >= pr.MinValue))
+                {
+                    count++;
+                } 
+                
+                // Console.WriteLine(string.Format("{0}, {1}, {2}, {3}", pr.MinValue, pr.MaxValue, pr.LetterRequired, pr.Password));
+            }
+
+            return count;
+        }
+
+        private static List<string> GetLineList(string fileName)
+        {
+            // file processing / control stuff
             List<string> lineList = new List<string>();
             int lineListCounter = 0;
             string line;
 
+            // read the file
             System.IO.StreamReader file = new System.IO.StreamReader(fileName);
             while ((line = file.ReadLine()) != null)
             {
@@ -38,16 +116,46 @@ namespace D02
                 lineListCounter++;
             }
 
-            List<PairedValues> elements = new List<PairedValues>();
+            return lineList;
+        }
 
-            for(int i = 0; i < lineList.Count +1; i++){
+        private static List<PairedValues> GetPairedValuesList(List<String> lineList)
+        {
+            List<PairedValues> pairedValuesList = new List<PairedValues>();
+
+            // Gets each element and put it in a "per-line" object
+            for (int i = 0; i <= lineList.Count -1; i++)
+            {
                 string[] subs = lineList[i].ToString().Split(' ');
                 PairedValues pv = new PairedValues(subs[0], subs[1], subs[2]);
-                elements.Add(pv);
+                pairedValuesList.Add(pv);
             }
 
+            return pairedValuesList;
+        }
 
-            return result;
+        private static List<PasswordRecord> GeneratePasswordRecords(List<PairedValues> pairedValuesList)
+        {
+            List<PasswordRecord> passwordRecords = new List<PasswordRecord>();
+
+            // puts each element in to a password record
+            for (int i = 0; i <= pairedValuesList.Count - 1; i++)
+            {
+                int minValue = 0;
+                int maxValue = 0;
+
+                string[] numbers = pairedValuesList[i].value1.Split("-");
+                char letterRequired = pairedValuesList[i].value2[0];
+                string password = pairedValuesList[i].value3;
+
+                minValue = Int32.Parse(numbers[0]);
+                maxValue = Int32.Parse(numbers[1]);
+
+                PasswordRecord pr = new PasswordRecord(minValue, maxValue, letterRequired, password);
+                passwordRecords.Add(pr);
+            }
+            
+            return passwordRecords;
         }
     }
 }
